21c025f39089012d8cbc5cfaee6bba5573332caa
Aswath <aswaththiru@gmail.com>
2023-01-09 09:42:50 +0530

    source code

---
+++ Bank.java
@@ -0,0 +1,24 @@
+package jan5;
+
+class Bank {
+    String name;
+    int ID;
+    int Phone;
+    private long bankTotalFund;
+    private float bankInterest;
+
+    public Bank(long bankTotalFund, float bankInterest) {
+        this.bankTotalFund = bankTotalFund;
+        this.bankInterest = bankInterest;
+
+    }
+
+    float checkLoan(int amount) {
+        if (amount < bankTotalFund) {
+            return bankInterest;
+        } else
+            return -1;
+
+    }
+
+}
