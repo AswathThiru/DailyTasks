395604d2f7c4d6789eac15a5d884aebdd4c522c9
Aswath <aswaththiru@gmail.com>
2023-01-19 09:16:15 +0530

    source code

---
+++ Firstprogram.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+ // var name="Aswath"; 
+    var name:String ="Aswath";
+    println("Yo enter age");
+    var age:Int = Integer.valueOf(readLine());
+    var age2:Long =age.toLong();
+// println("Yohohohoh " + name);
+println("Yohohohoh $name is $age2 years old");
+}
+fun add(){
+    println("Yo");
+}
