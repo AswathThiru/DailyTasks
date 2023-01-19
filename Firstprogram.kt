f0d9eb5556909a5d97da4931062add339f504de2
Aswath <aswaththiru@gmail.com>
2023-01-19 09:17:10 +0530

    source code

--- Firstprogram.kt
+++ Firstprogram.kt
@@ -4,7 +4,7 @@ fun main(args: Array<String>) {
     println("Yo enter age");
     var age:Int = Integer.valueOf(readLine());
     var age2:Long =age.toLong();
-// println("Yohohohoh " + name);
+//  println("Yohohohoh " + name);
 println("Yohohohoh $name is $age2 years old");
 }
 fun add(){
